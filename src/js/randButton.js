b47c0987508b6041b68ba90bc38ed1034888f58c
acast23 <antoniocastillob@hotmail.com>
2021-08-18 23:49:06 -0500

    lastchanges
    cambios search

--- src/js/randButton.js
+++ src/js/randButton.js
@@ -1,46 +1,103 @@
 const results = document.getElementById('results');
+
+
 const randomButton = document.getElementById('randomBtn');
+const searchButton = document.getElementById('searchBtn');
 
 randomButton.addEventListener('click', searchRand);
+searchButton.addEventListener('click', search);
 
 function searchRand() {
-fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-.then(function (response) {
-return response.json();
-})
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(function (response) {
+            return response.json();
+        })
 
-.then(function (data) {
-//console.log(data);
+        .then(function (data) {
+            //console.log(data);
 
-let meal = data.meals[0] //obtiene primer elem de array
+            let meal = data.meals[0] //obtiene primer elem de array
 
-let title = meal.strMeal   //titulo
-let inst = meal.strInstructions //instrucciones
-let img = meal.strMealThumb  //imagen
+            let title = meal.strMeal //titulo
+            let inst = meal.strInstructions //instrucciones
+            let img = meal.strMealThumb //imagen
 
-let ingArray = []  //Array donde se guardaran los ingredientes
+            let ingArray = [] //Array donde se guardaran los ingredientes
 
-for (let i=1; i<=20;i++){   //como cada ingrediente es un elemento del array distinto solo cambiando el numero final, se usa un for loop
-    if(meal[`strIngredient${i}`]){
-        ingArray.push(meal[`strIngredient${i}`])  //solo mete al array donde ingredientes contenga texto
-    }
-    else break;
-}
+            for (let i = 1; i <= 20; i++) { //como cada ingrediente es un elemento del array distinto solo cambiando el numero final, se usa un for loop
+                if (meal[`strIngredient${i}`]) {
+                    ingArray.push(meal[`strIngredient${i}`]) //solo mete al array donde ingredientes contenga texto
+                } else break;
+            }
 
-//se crea la estructura html para luego meterla al div con id results
-results.innerHTML = `
+            //se crea la estructura html para luego meterla al div con id results
+            results.innerHTML = `
 
-<h3>${title}</h3>
-<img src="${img}" alt="${title}">
-<div>${inst}<div>
-<div>
-    <h5>Ingredientes:</h5>
-				<ul>
-					${ingArray.map(ingredient => `<li>${ingredient}</li>`).join('')}
-				</ul>
-</div>`
-  })
+                            <h3>${title}</h3>
+                            <img src="${img}" alt="${title}">
+                            <div>${inst}<div>
+                            <div>
+                                <h5>Ingredientes:</h5>
+                                            <ul>
+                                                ${ingArray.map(ingredient => `<li>${ingredient}</li>`).join('')}
+                                            </ul>
+                            </div>`
+                                    })
+                            }
 
 
+function search() {
+    let searchInput = document.getElementById('searchInput').value;
 
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+                //console.log(data)
+
+                let meals = data.meals
+                let bloques = []
+
+                if(meals == null){
+                    results.innerHTML = '<h3 class="mt-4 mb-5">No se encontraron resultados</h3>'
+                }
+                else{
+                
+
+                let gridHTML = `<h3 class="mt-4 mb-5">Resultados de la busqueda</h3>    
+                                <div class="row row-cols-1 row-cols-md-4 g-4" id= "grid">`
+
+                bloques.push(gridHTML)
+
+                for (let i = 0; i < meals.length; i++) {
+
+                    let meal = meals[i]
+                    let title = meal.strMeal //titulo
+                    let img = meal.strMealThumb //imagen
+
+                    let currentHTML =
+                        `
+                <div class="mb-4 col">
+                <div class="card border-0 rounded-3 bg-light text-center h-100">
+                    <a href="#" class="text-decoration-none text-reset">
+                        <img src="${img}" class="card-img-top"
+                            alt="${title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${title}</h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+                            `
+                    bloques.push(currentHTML)
+                }
+
+                 gridHTML ='</div>'
+                 bloques.push(gridHTML)
+
+            results.innerHTML = bloques.join('')
+                }
+        })
 }
