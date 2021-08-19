f2d0e767d5ed444526afe8c1a6480bd8c3189e53
Ana Castillo <66333350+canastillo@users.noreply.github.com>
2021-08-18 22:54:45 -0500

    Fix conflicto deestilos recetas e ingredientes

--- src/js/main.js
+++ src/js/main.js
@@ -9,24 +9,25 @@ fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
     .then(data => {
         data['meals'].forEach(receta => {
             console.log(receta)
-            let card = createCardCategoria(receta);
-            recetasContenedor.appendChild(card);
+            let card = createCardCategoria(receta)
+            recetasContenedor.appendChild(card)
         })
     })
-    .catch(error => console.log(error));
+    .catch(error => console.log(error))
 
 const createCardCategoria = (receta) =>{
     let card = document.createElement("div")
-    card.classList.add("card");
+    card.classList.add("card")
+    card.classList.add("card-recipe")
 
     let imagenReceta = document.createElement("img")
     imagenReceta.src = receta["strMealThumb"]
 
-    let nombrePlatillo = document.createElement("h2");
-    nombrePlatillo.textContent = receta["strMeal"];
+    let nombrePlatillo = document.createElement("h2")
+    nombrePlatillo.textContent = receta["strMeal"]
 
-    card.appendChild(imagenReceta);
-    card.appendChild(nombrePlatillo);
+    card.appendChild(imagenReceta)
+    card.appendChild(nombrePlatillo)
 
     return card
 }
@@ -37,9 +38,9 @@ fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list')
     .then(response => response.json())
     .then(data => {
         for(let i=0; i<4; i++){
-            let ingrediente = data['meals'][i];
+            let ingrediente = data['meals'][i]
             let card = createCardIngrediente(ingrediente)
-            ingredientesContenedor.appendChild(card);
+            ingredientesContenedor.appendChild(card)
         }
     })
     .catch(error => console.log(error));
@@ -48,17 +49,18 @@ const createCardIngrediente = (ingrediente) =>{
     let card = document.createElement("div")
     let nombreIngrediente = ingrediente["strIngredient"]
     
-    card.classList.add("card");
+    card.classList.add("card")
+    card.classList.add("card-ingredient")
 
     let link = document.createElement("a")
     link.href = `ingredient.html?i=${nombreIngrediente}`
     link.classList.add("text-decoration-none")
     link.classList.add("text-reset")
 
-    let tituloIngrediente = document.createElement("h2");
-    tituloIngrediente.textContent = nombreIngrediente;
+    let tituloIngrediente = document.createElement("h2")
+    tituloIngrediente.textContent = nombreIngrediente
 
-    let imagenIngrediente = document.createElement("img");
+    let imagenIngrediente = document.createElement("img")
     let urlImagen = `https://www.themealdb.com/images/ingredients/${nombreIngrediente}.png`
     imagenIngrediente.src = urlImagen
 
